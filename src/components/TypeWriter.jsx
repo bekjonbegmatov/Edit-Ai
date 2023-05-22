70ebccbd41d9d7adedb53bbbcb5669d9a3a5fd98
bekjonbegmatov <behruzbegmatov28@@gmail.com>
2023-05-22 21:57:25 +0500

    coding and fix some bugs

--- src/components/TypeWriter.jsx
+++ src/components/TypeWriter.jsx
@@ -5,10 +5,12 @@ const Typewriter = ({ text, delay }) => {
   const [displayedText, setDisplayedText] = useState('');
   
   useEffect(() => {
+    let tex = text
+    let replacedText = tex.replace(/</g, '&lt;').replace(/>/g, '&gt;');
     let i = 0;
     const intervalId = setInterval(() => {
-      if (i < text.length) {
-        setDisplayedText(displayedText => displayedText + text.charAt(i));
+      if (i < replacedText.length) {
+        setDisplayedText(displayedText => displayedText + replacedText.charAt(i));
         i++;
       } else {
         clearInterval(intervalId);
--- src/components/messages.jsx
+++ src/components/messages.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Typewriter from './TypeWriter';
 import immage from '../assets/edith.png'
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
 import './messages.css'
 function Messages(props) {
     return (
@@ -8,10 +12,14 @@ function Messages(props) {
             {!props.is_user &&
                 <div className="chatgp">
                     <img src={immage} width='40px' alt="" />
-
                     <div className="alert alert-success chat_message" role="alert">
-                        <Typewriter  text={props.text} delay={25} />
+                        <Typewriter text={props.text} delay={25} />
+
+                        {/* <SyntaxHighlighter language="javascript" style={docco}>
+                            <Typewriter text={props.text} delay={25} />
+                        </SyntaxHighlighter> */}
                     </div>
+
                 </div>
             }{props.is_user &&
                 <div className='user'>
